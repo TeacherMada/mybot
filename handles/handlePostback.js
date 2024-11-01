ed6b53fb9fbfef19818eb56b91d84bb20300cbde
Tsanta Rabemananjara <79659279+Band000@users.noreply.github.com>
2024-11-01 16:44:27 +0300

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -7,7 +7,7 @@ const handlePostback = async (event, pageAccessToken) => {
   if (!senderId || !payload) return console.error('Invalid postback event object');
 
   try {
-    await sendMessage(senderId, { text: `Bonjour | Welcome \n\n▪︎Raha hijery Menu dia soraty: Menu \n▪︎Raha hanamboarana Chatbot Professionnel dia: 0349310268\n\n 🙋‍♂️TsantaBot: \nBon, Comment puis-je vous aider aujourd'hui ?` }, pageAccessToken);
+    await sendMessage(senderId, { text: `Bonjour | Welcome \n\n▪︎Raha hijery Menu dia soraty: help \n\n▪︎Raha hanamboarana Chatbot Professionnel dia: 0349310268\n\n 🙋‍♂️| TsantaBot: \nBon, Comment puis-je vous aider aujourd'hui ?` }, pageAccessToken);
   } catch (err) {
     console.error('Error sending postback response:', err.message || err);
   }
