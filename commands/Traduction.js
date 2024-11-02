dc8241dc9d606be5bd92fb61e2265a97a729e95d
Tsanta Rabemananjara <79659279+Band000@users.noreply.github.com>
2024-11-02 14:21:59 +0300

    Update Traduction.js

--- commands/Traduction.js
+++ commands/Traduction.js
@@ -8,10 +8,10 @@ const token = fs.readFileSync('token.txt', 'utf8');
 const userTranslationRequests = {};
 
 module.exports = {
-  name: 'trans',
-  description: 'Traduire un texte vers une langue cible',
-  author: 'Tata',
-  usage: 'trans [texte à traduire]',
+  name: 'traduction',
+  description: 'Traduire un texte vers une langue désirée',
+  author: 'Tsanta',
+  usage: 'Traduction [texte à traduire]',
 
   async execute(senderId, args) {
     const pageAccessToken = token;
@@ -19,21 +19,62 @@ module.exports = {
 
     // Vérifie si du texte est fourni pour la traduction
     if (!content) {
-      return await sendMessage(senderId, { text: 'Veuillez fournir un texte à traduire.' }, pageAccessToken);
+      return await sendMessage(senderId, { text: 'Veuillez fournir un texte à traduire. \n ▪︎Ex: Traduction I love you' }, pageAccessToken);
     }
 
-    // Liste des langues disponibles
+    // Liste complète des langues disponibles
     const languages = [
+      { title: 'Malagasy', code: 'mg' },
       { title: 'Français', code: 'fr' },
       { title: 'Anglais', code: 'en' },
+      { title: 'Arabic', code: 'ar' },
+      { title: 'Chinois', code: 'zh' },
       { title: 'Espagnol', code: 'es' },
       { title: 'Allemand', code: 'de' },
-      { title: 'Italien', code: 'it' },
       { title: 'Japonais', code: 'ja' },
-      { title: 'Chinois', code: 'zh' },
-      { title: 'Arabe', code: 'ar' },
-      { title: 'Russe', code: 'ru' },
-      { title: 'Portugais', code: 'pt' },
+      { title: 'Croatian', code: 'hr' },
+      { title: 'Czech', code: 'cs' },
+      { title: 'Danish', code: 'da' },
+      { title: 'Dutch', code: 'nl' },
+      { title: 'Estonian', code: 'et' },
+      { title: 'Filipino', code: 'fil' },
+      { title: 'Tagalog', code: 'tl' },
+      { title: 'Finnish', code: 'fi' },
+      { title: 'German', code: 'de' },
+      { title: 'Greek', code: 'el' },
+      { title: 'Gujarati', code: 'gu' },
+      { title: 'Hindi', code: 'hi' },
+      { title: 'Hungarian', code: 'hu' },
+      { title: 'Icelandic', code: 'is' },
+      { title: 'Indonesian', code: 'id' },
+      { title: 'Italian', code: 'it' },
+      { title: 'Japanese', code: 'ja' },
+      { title: 'Kannada', code: 'kn' },
+      { title: 'Khmer', code: 'km' },
+      { title: 'Korean', code: 'ko' },
+      { title: 'Latin', code: 'la' },
+      { title: 'Latvian', code: 'lv' },
+      { title: 'Malayalam', code: 'ml' },
+      { title: 'Marathi', code: 'mr' },
+      { title: 'Nepali', code: 'ne' },
+      { title: 'Norwegian', code: 'nb' },
+      { title: 'Polish', code: 'pl' },
+      { title: 'Portuguese', code: 'pt' },
+      { title: 'Romanian', code: 'ro' },
+      { title: 'Russian', code: 'ru' },
+      { title: 'Serbian', code: 'sr' },
+      { title: 'Sinhalese', code: 'si' },
+      { title: 'Slovak', code: 'sk' },
+      { title: 'Spanish', code: 'es' },
+      { title: 'Swahili', code: 'sw' },
+      { title: 'Swedish', code: 'sv' },
+      { title: 'Tamil', code: 'ta' },
+      { title: 'Telugu', code: 'te' },
+      { title: 'Thai', code: 'th' },
+      { title: 'Turkish', code: 'tr' },
+      { title: 'Ukrainian', code: 'uk' },
+      { title: 'Urdu', code: 'ur' },
+      { title: 'Vietnamese', code: 'vi' },
     ];
 
     // Crée les quick replies pour chaque langue
@@ -48,7 +89,7 @@ module.exports = {
 
     // Envoie un message avec les options de langues
     await sendMessage(senderId, {
-      text: "Traduire en :",
+      text: "▪︎Traduire en (sélectionnez):",
       quick_replies: quickReplies
     }, pageAccessToken);
   },
@@ -86,7 +127,7 @@ module.exports = {
       });
 
       // Envoi du message avec la traduction
-      const formattedMessage = `Traduction : ${translatedText}`;
+      const formattedMessage = `✅ Traduction :\n\n ${translatedText}`;
       await sendMessage(senderId, { text: formattedMessage }, pageAccessToken);
 
       // Supprime la requête de traduction après l'envoi
