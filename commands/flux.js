eef6190758452f08aff2998b3a37757c42053ffa
Tsanta Rabemananjara <79659279+Band000@users.noreply.github.com>
2025-02-27 11:23:29 +0300

    Update flux.js

--- commands/flux.js
+++ commands/flux.js
@@ -22,7 +22,7 @@ module.exports = {
 
     // Concatenate arguments to form the prompt
     const prompt = args.join(' ');
-    const apiUrl = `https://api.zetsu.xyz/api/flux?prompt=${encodeURIComponent(prompt)}&model=4`;  // API endpoint with the prompt
+    const apiUrl = `https://kaiz-apis.gleeze.com/api/flux?prompt=${encodeURIComponent(prompt)}&model=4`;  // API endpoint with the prompt
 
     // Notify user that the image is being generated
     await sendMessage(senderId, { text: '⌛ Attendez svp ...' }, pageAccessToken);
