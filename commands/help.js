9d481751362eb53bbf8eb97ac79235d79e92323a
TeacherMada <99089646+TeacherMada@users.noreply.github.com>
2026-02-17 21:12:45 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const { sendMessage } = require('../handles/sendMessage');
 
-// 🔐 Liste des Admins autorisés (séparés par virgule dans .env)
+export const name = "admin";
+
+// Liste des Admins autorisés (peut être plusieurs séparés par une virgule)
 const ADMINS = (process.env.ADMIN_ID || "")
   .split(",")
   .map(a => a.trim());
@@ -12,68 +14,52 @@ module.exports = {
   description: 'Liste commandes',
   usage: 'help [commande]',
   author: 'System',
-
   execute(senderId, args, pageAccessToken) {
-
-    // 🔐 Vérification Admin
-    if (!ADMINS.includes(senderId.toString())) {
-      return sendMessage(
-        senderId,
-        { text: "❌ Accès refusé." },
-        pageAccessToken
-      );
-    }
+      // Vérification admin
+  if (!ADMINS.includes(senderId.toString())) {
+    return sendMessage(senderId, { text: "❌ Accès refusé." }, pageAccessToken);
+  }
 
     const commandsDir = path.join(__dirname, '../commands');
     const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
-    // 📌 Si help [commande]
     if (args.length > 0) {
-
       const commandName = args[0].toLowerCase();
-
       const commandFile = commandFiles.find(file => {
         const command = require(path.join(commandsDir, file));
         return command.name.toLowerCase() === commandName;
       });
 
       if (commandFile) {
-
         const command = require(path.join(commandsDir, commandFile));
-
         const commandDetails = `
 ▪︎Commande: ${command.name}
-▪︎Description: ${command.description || "Aucune description"}
-▪︎Usage: ${command.usage || "Non spécifié"}
-`;
-
-        return sendMessage(senderId, { text: commandDetails }, pageAccessToken);
-
+▪︎Description: ${command.description}
+▪︎Usage: ${command.usage}`;
+        
+        sendMessage(senderId, { text: commandDetails }, pageAccessToken);
       } else {
-        return sendMessage(
-          senderId,
-          { text: `❌ Aucune commande "${commandName}" trouvée.` },
-          pageAccessToken
-        );
+        sendMessage(senderId, { text: `Aucune commande "${commandName}" trouvé`}, pageAccessToken);
       }
+      return;
     }
 
-    // 📌 Liste complète
     const commands = commandFiles.map(file => {
       const command = require(path.join(commandsDir, file));
       return `│─➤ ${command.name}`;
     });
 
     const helpMessage = `
-🔐 TsantaBot – Commandes Admin
+ TsantaBot Commandes disponibles 
 ╭──○○○
 ${commands.join('\n')}
 ╰──────────○
-
 #Aide:
 ▪︎help [commande]
+▪︎Contact: 0349310268
+▪︎Fb: https://www.facebook.com/profile.php?id=61552825191002
 `;
 
-    return sendMessage(senderId, { text: helpMessage }, pageAccessToken);
+    sendMessage(senderId, { text: helpMessage }, pageAccessToken);
   }
 };
